f4d3cb62671bddc4a5ff63b0882208831071aee8
NaiduBagana <baganasatyavathi@gmail.com>
2024-04-06 14:31:01 +0530

    new commit

---
+++ As.py
@@ -0,0 +1,6 @@
+#Start your searching journey by tracing the code snippet given below .All The Very Best...........
+li=['A','k','a','l','r','m','a','n','m','o','b','p','h']
+str=""
+for i in range(0,len(li),2):
+    str=str+li[i]
+print(str)
